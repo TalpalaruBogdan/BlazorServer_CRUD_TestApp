2bef11365750b49d17e1ba28de6ce09c98d86250
bogdan.talpalaru <bogdan.talpalaru@avocodo.com>
2023-03-07 11:07:27 +0200

    Add project files.

---
+++ TestApp/Data/Context/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApp.Data.Models;
+
+namespace TestApp.Data.Context
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ TestApp/Data/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace TestApp.Data.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public float Price { get; set; }
+    }
+}
